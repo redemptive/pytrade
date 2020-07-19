998afef06a4b11b0a3708bb4a7f553cdce3ab004
redemptive <ewan_forbes@yahoo.co.uk>
2020-07-19 18:34:24 +0100

    Small improvements to the livetrading object

--- obj/Data.py
+++ obj/Data.py
@@ -6,6 +6,26 @@ from datetime import datetime
 class Data:
 
     @staticmethod
+    def process_socket_data(message):
+        # This converts the kline from the socket stream to the ohclv data
+        # so it is the same as the backtesting historical data returned from API
+        # which is what a Strategy accepts
+        return [
+            message['t'],  # Open time
+            message['o'],  # Open
+            message['h'],  # High
+            message['l'],  # Low
+            message['c'],  # Close
+            message['v'],  # Volume
+            message['T'],  # Close time
+            message['q'],  # Quote asset volume
+            message['n'],  # Number of trades
+            message['V'],  # Taker buy base asset volume
+            message['Q'],  # Taker buy quote asset volume
+            message['B']  # Ignore
+        ]
+
+    @staticmethod
     def process_raw_historic_data(klines):
         data = pd.DataFrame(klines, columns=[
             "open_time",
